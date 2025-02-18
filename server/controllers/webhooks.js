34b2ccfcf9250755b67605252357fef26e57e4cb
umer142 <umersjafiq79@gmail.com>
2025-02-18 06:05:01 +0500

    six-commit

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -55,6 +55,6 @@ export const clerkWebhooks = async (req, res) => {
     }
   } catch (error) {
     console.log(error.message);
-    res.json({ success: false, message: "Webhooks Error" });
-  }
+    res.json({ success: false, message: "Webhooks Error"});
+}
 };
