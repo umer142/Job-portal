6a02f54c6dbd4b59168ddcc5c5c2f61a0e90d0c3
umer142 <umersjafiq79@gmail.com>
2025-02-24 08:53:43 +0500

    stringfy

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  clerkId: { type: String, required: true, unique: true }, // Add Clerk ID field
+  clerkId: { type: String, required: true, unique: true }, // Store Clerk ID separately
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   resume: { type: String, default: "" },
