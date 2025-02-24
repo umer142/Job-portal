9eef4c9d0a8871774f906a87f3553fcff650c18b
umer142 <umersjafiq79@gmail.com>
2025-02-24 08:11:49 +0500

    User-commit

--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
 
-const  userSchema = new mongoose.Schema({
-    _id: {type:String, required:true},
-    name:{type:String, required:true},
-    email:{type:String, required:true, unique:true},
-    resume:{type:String},
-    image:{type:String, required:true}
-})
+const userSchema = new mongoose.Schema({
+  _id: { type: String, required: true }, // Clerk ID as _id
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  resume: { type: String, default: "" }, // Default empty string for optional fields
+  image: { type: String, required: true },
+});
 
-
-const User = mongoose.model('user',userSchema)
-
+const User = mongoose.model("User", userSchema);
 
 export default User;
